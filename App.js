87245920da167ef3bbc6787b7e0b955c20491da8
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-06-18 11:26:24 +0300

    learned Flatlist, ScrollView, Button, TextInput, Text, View, Stylesheet

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+  FlatList
+} from 'react-native';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [goalsArray, setGoalsArray] = useState([]);
+
+  const inputHandler = (enteredText) => {
+    setInputValue(enteredText);
+  };
+
+  const buttonHandler = () => {
+    if (inputValue.trim().length === 0) {
+      return;
+    }
+    setGoalsArray((prevState) => [...prevState, {id: Math.random().toString(), inputValue}]);
+    setInputValue('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>This is just a BEGINING</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={styles.inputButton}>
+          <TextInput
+            placeholder="Your course goal!"
+            style={styles.inp}
+            onChangeText={inputHandler}
+            value={inputValue}
+          />
+          <Button title="Add goal" onPress={buttonHandler} />
+        </View>
+      </TouchableWithoutFeedback>
+      <View style={styles.list}>
+        <FlatList data={goalsArray} keyExtractor={(item) => item.id} renderItem={itemData => {
+          return (
+              <View style={styles.goalWrapper}>
+                <Text style={styles.listText}>{itemData.item.inputValue}</Text>
+              </View>
+          )
+        }} alwaysBounceVertical={true} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  inputButton: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    flex: 1,
+  },
+  inp: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
+    width: '77%',
+  },
+  list: {
+    flex: 5,
+  },
+  goalWrapper: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 7,
+    backgroundColor: '#5e0acc',
+  },
+  listText: {
+    color: 'white',
   },
 });
