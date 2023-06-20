00a19c7b23ce3c47502497eac67cbc8e11efa6c4
Micle Dev <mickle.develop@gmail.com>
2023-06-20 10:58:35 +0300

    onto building first game

--- components/GoalInput.jsx
+++ components/GoalInput.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 import {
   StyleSheet,
@@ -8,22 +8,25 @@ import {
   TextInput,
   Button,
   Modal,
-  Image
-} from 'react-native';
+  Image,
+} from "react-native";
 
 export default function GoalInput({ btnHand, closeModal, modalSt }) {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const inputHandler = (enteredText) => {
     setInputValue(enteredText);
   };
 
   return (
-    <Modal visible={modalSt} animationType="slide" >
+    <Modal visible={modalSt} animationType="slide">
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
           <View style={styles.input}>
-            <Image source={require('../assets/image/goal.png')} style={styles.image}/>
+            <Image
+              source={require("../assets/image/goal.png")}
+              style={styles.image}
+            />
             <TextInput
               placeholder="Your course goal!"
               style={styles.inp}
@@ -33,19 +36,19 @@ export default function GoalInput({ btnHand, closeModal, modalSt }) {
           </View>
         </TouchableWithoutFeedback>
         <View style={styles.btnsCont}>
-        <Button
+          <Button
             title="Back to list"
             onPress={() => {
-              setInputValue(''), closeModal(false);
+              setInputValue(""), closeModal(false);
             }}
-            color='#f31282'
+            color="#f31282"
           />
           <Button
             title="Add goal"
             onPress={() => {
-              btnHand(inputValue), setInputValue('');
+              btnHand(inputValue), setInputValue("");
             }}
-            color='#b180f0'
+            color="#b180f0"
           />
         </View>
       </View>
@@ -56,35 +59,35 @@ export default function GoalInput({ btnHand, closeModal, modalSt }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#311b6b',
-    alignItems: 'center'
+    backgroundColor: "#311b6b",
+    alignItems: "center",
   },
   input: {
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
     paddingHorizontal: 30,
     flex: 1,
     marginBottom: 15,
-    width: '100%'
+    width: "100%",
   },
   inp: {
-    borderColor: '#e4d0ff',
-    backgroundColor: '#e4d0ff',
-    color: '#120438',
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
     borderRadius: 7,
     borderWidth: 1,
     padding: 10,
   },
   btnsCont: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     gap: 30,
   },
   image: {
     width: 100,
     height: 100,
     marginBottom: 20,
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  }
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
 });
